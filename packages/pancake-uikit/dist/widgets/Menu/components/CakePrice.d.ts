6cfffb78ca7de82f9bb668e510fad0da25bbffa9
Swift Finance <87555534+PolySwift@users.noreply.github.com>
2021-09-10 20:23:54 +0800

    Add files via upload

--- packages/pancake-uikit/dist/widgets/Menu/components/CakePrice.d.ts
+++ packages/pancake-uikit/dist/widgets/Menu/components/CakePrice.d.ts
@@ -1,6 +1,7 @@
 import React from "react";
 interface Props {
     cakePriceUsd?: number;
+    isDark: boolean;
 }
 declare const _default: React.NamedExoticComponent<Props>;
 export default _default;

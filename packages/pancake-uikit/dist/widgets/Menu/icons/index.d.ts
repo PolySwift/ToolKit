583879d68f5d61c69628c5511a9ead4e54c75fa5
Swift Finance <87555534+PolySwift@users.noreply.github.com>
2021-09-17 18:59:10 +0800

    Add files via upload

--- packages/pancake-uikit/dist/widgets/Menu/icons/index.d.ts
+++ packages/pancake-uikit/dist/widgets/Menu/icons/index.d.ts
@@ -17,3 +17,4 @@ export { default as TelegramIcon } from "./Telegram";
 export { default as TicketIcon } from "./Ticket";
 export { default as TradeIcon } from "./Trade";
 export { default as TwitterIcon } from "./Twitter";
+export { default as NomicsIcon } from "./Nomics";
